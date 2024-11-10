603edefdfa91cf5df6d3246e9c72477061fc04d4
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-11-10 15:35:30 +0000

    Edit db sql

--- backend/src/db/db.sql
+++ backend/src/db/db.sql
@@ -1,4 +1,16 @@
 -- drop database if exists webtech_fall2024_madiba_quansah;
+-- drop table if exists feedback;
+-- drop table if exists payment;
+-- drop table if exists order_details;
+-- drop table if exists `order`;
+-- drop table if exists menu_item_inventory;
+-- drop table if exists inventory;
+-- drop table if exists menu_item;
+-- drop table if exists staff;
+-- drop table if exists reservations;
+-- drop table if exists customer;
+-- drop table if exists `table`;
+-- drop table if exists admin;
 CREATE DATABASE if not exists webtech_fall2024_madiba_quansah;
 
 USE webtech_fall2024_madiba_quansah;
