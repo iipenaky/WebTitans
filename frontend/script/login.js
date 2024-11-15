03d4ff4a4deb8c3df6858837bf600430d13d949d
Yaw Twumasi Agyeman-Budu <agyayaw2002@gmail.com>
2024-11-15 16:49:05 +0000

    Login integration example

---
+++ frontend/script/login.js
@@ -0,0 +1,44 @@
+import { BASE_URL } from "./constants.js";
+const loginForm = document.getElementById("loginForm")
+
+async function loginUser(e) {
+    e.preventDefault();
+    if(!validateLogin()) return;
+
+    const formData = new FormData(loginForm);
+
+    const req = await fetch(`${BASE_URL}/user/login`, {method: "POST", body: JSON.stringify(Object.fromEntries(formData))})
+
+    if(!req.ok) {
+        console.log({req});
+        alert("Login Error! Please try again")
+    }
+
+    const json = await req.json();
+    console.log({json});
+
+
+}
+
+function validateLogin() {
+    let isValid = true;
+    const email = document.getElementById('loginEmail').value.trim();
+    const password = document.getElementById('loginPassword').value.trim();
+
+    document.getElementById('emailError').classList.add('hidden');
+    document.getElementById('passwordError').classList.add('hidden');
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        document.getElementById('emailError').classList.remove('hidden');
+        isValid = false;
+    }
+
+    if (password === '') {
+        document.getElementById('passwordError').classList.remove('hidden');
+        isValid = false;
+    }
+    return isValid;
+}
+
+loginForm.onsubmit = loginUser;
