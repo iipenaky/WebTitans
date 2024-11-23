8386131ae12dfb299ed419e530a0a51044b434b4
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-11-23 11:38:01 +0000

    Remove payment route

--- backend/src/routes/admin.php
+++ backend/src/routes/admin.php
@@ -17,9 +17,9 @@ $AdminService = new AdminService;
 function signUp($data)
 {
     global $AdminService;
-    $fields = ['username', 'password'];
+    $fields = ['username', 'password', 'token'];
     if (validateData($fields, $data, 'sign up')) {
-        $res = $AdminService->SignUp($data['username'], $data['password']);
+        $res = $AdminService->SignUp($data['username'], $data['password'], $data['token']);
         header($res['header']);
         echo json_encode($res['data']);
     }
@@ -98,12 +98,6 @@ function adminHandler($verb, $subroute)
                 break;
             }
             customersHandler($verb, $subroute);
-            break;
-        case 'payments':
-            if (! checkAuth()) {
-                break;
-            }
-            paymentsHandler($verb, $subroute);
             break;
         case 'orders':
             if (! checkAuth()) {
